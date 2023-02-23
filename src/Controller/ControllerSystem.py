b85dddcd1ed188180af44d6dff3762f52ff037c0
Jeffrey <77903214+jgrospe92@users.noreply.github.com>
2023-02-23 15:21:28 -0500

    fix connition with the gpio

--- src/Controller/ControllerSystem.py
+++ src/Controller/ControllerSystem.py
@@ -1,6 +1,8 @@
 # This class handles all functionalities when connecting to the Raspberry
 
 # Import Rpi and sleep libraris
+import RPi.GPIO as GPIO
+from time import sleep
 # Set a global flag
 isActive = 0 # this tells the program if the light is on or off
 
