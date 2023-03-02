d41a3f15355e73ad88ab609800083cb4aa9a9e72
Jeffrey <jeffreygrospe@yahoo.com>
2023-03-01 19:35:54 -0500

    remove updating status on the browser

--- app.py
+++ app.py
@@ -12,17 +12,20 @@ import src.Controller.ControllerSystem as cs
 # dbc.themes.SLATE = this is a dark faded theme
 
 def main() -> None:
-    app = Dash(__name__,external_stylesheets=[dbc.themes.BOOTSTRAP, dbc.themes.VAPOR])
-    app.title = "IOT DashBoard"
-    app.layout = create_layout(app)
     print("Iniating Dashboard...")
     '''
         WHen using Raspberry pi and GPIO
         NOTE: Uncomment this cs.set_up()
         NOTE: Also go to the ControllerSystem.py, uncomment the GPIO.output in the light_controller()
         NOTE: Lastly, uncomment import RPi.GPIO as GPIO and from time import sleep
+        
+        When you just want to work on the Dashboard, comment out cs.set_up()
     '''
     #cs.set_up()
+
+    app = Dash(__name__, update_title=None, external_stylesheets=[dbc.themes.BOOTSTRAP, dbc.themes.VAPOR])
+    app.title = "IOT DashBoard"
+    app.layout = create_layout(app)
 
     app.run()
 
