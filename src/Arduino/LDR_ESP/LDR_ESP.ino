788b9edd34049944f57495f24f962036f3fc4da2
jgrospe92 <jeffreygrospe@yahoo.com>
2023-04-19 15:12:19 -0400

    add rfid script to the broker script

--- src/Arduino/LDR_ESP/LDR_ESP.ino
+++ src/Arduino/LDR_ESP/LDR_ESP.ino
@@ -1,8 +1,24 @@
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
-const char* ssid = "TP-Link_2AD8";
-const char* password = "14730078";
-const char* mqtt_server = "192.168.0.149";
+
+// RFID
+#include <SPI.h>
+#include <MFRC522.h>
+#define SS_PIN D8
+#define RST_PIN D0
+MFRC522 rfid(SS_PIN, RST_PIN); // Instance of the class
+MFRC522::MIFARE_Key key;
+// Init array that will store new NUID
+byte nuidPICC[4];
+// card id
+String card_id = "";
+
+//const char* ssid = "TP-Link_2AD8";
+const char* ssid = "peacewalker\342\200\231s iPhone";
+//const char* password = "14730078";
+const char* password = "iotlab123";
+//const char* mqtt_server = "192.168.0.148";
+const char* mqtt_server = "172.20.10.3";
 WiFiClient vanieriot;
 PubSubClient client(vanieriot);
 void setup_wifi() {
@@ -53,11 +69,68 @@ void setup() {
  setup_wifi();
  client.setServer(mqtt_server, 1883);
  client.setCallback(callback);
+ // setup for the rfid
+ SPI.begin(); // Init SPI bus
+ rfid.PCD_Init(); // Init MFRC522
+ Serial.println();
+ Serial.print(F("Reader :"));
+ rfid.PCD_DumpVersionToSerial();
+ for (byte i = 0; i < 6; i++) {
+  key.keyByte[i] = 0xFF;
+ }
+ //Note: To use the RFID RC522 module we use the SPI.h library which will allow us to
+ //establish the communication between the ESP8266 card and the module; and the
+ //MFRC522.h library which will allow us to dialogue with the module.
+ Serial.println();
+ Serial.println(F("This code scan the MIFARE Classic NUID."));
+ Serial.print(F("Using the following key:"));
 }
 void loop() {
- int sensorValue = analogRead(A0);
- char sum[5];
- itoa(sensorValue, sum, 10);
+ //int sensorValue = analogRead(A0);
+ // char sum[5];
+ //itoa(sensorValue, sum, 10);
+ // start of rfid
+ // Reset the loop if no new card present on the sensor/reader. This saves the entire process when idle.
+if ( ! rfid.PICC_IsNewCardPresent())
+ return;
+// Verify if the NUID has been readed
+if ( ! rfid.PICC_ReadCardSerial())
+ return;
+Serial.print(F("PICC type: "));
+MFRC522::PICC_Type piccType = rfid.PICC_GetType(rfid.uid.sak);
+Serial.println(rfid.PICC_GetTypeName(piccType));
+// Check is the PICC of Classic MIFARE type
+if (piccType != MFRC522::PICC_TYPE_MIFARE_MINI &&
+ piccType != MFRC522::PICC_TYPE_MIFARE_1K &&
+ piccType != MFRC522::PICC_TYPE_MIFARE_4K) {
+ Serial.println(F("Your tag is not of type MIFARE Classic."));
+ return;
+}
+if (rfid.uid.uidByte[0] != nuidPICC[0] ||
+ rfid.uid.uidByte[1] != nuidPICC[1] ||
+ rfid.uid.uidByte[2] != nuidPICC[2] ||
+ rfid.uid.uidByte[3] != nuidPICC[3] ) {
+ Serial.println(F("A new card has been detected."));
+ // Store NUID into nuidPICC array
+ for (byte i = 0; i < 4; i++) {
+ nuidPICC[i] = rfid.uid.uidByte[i];
+ }
+ Serial.println(F("The NUID tag is:"));
+ Serial.println();
+ Serial.print(F("In dec: "));
+ printDec(rfid.uid.uidByte, rfid.uid.size);
+ card_id = String(rfid.uid.uidByte[0]) + String(rfid.uid.uidByte[1]) + String(rfid.uid.uidByte[2]) + String(rfid.uid.uidByte[3]);
+ Serial.println();
+ Serial.print("UID : " + card_id);
+}
+else Serial.println(F("Card read previously."));
+// Halt PICC
+rfid.PICC_HaltA();
+// Stop encryption on PCD
+rfid.PCD_StopCrypto1();
+
+ // -- end of rfid
+ 
  if (!client.connected()) {
  reconnect();
  }
@@ -65,7 +138,19 @@ void loop() {
   client.connect("vanieriot");
 
 
-  client.publish("IoTlab/ESP",sum);
+  //client.publish("IoTlab/ESP",sum);
 
  delay(5000);
  }
+
+ /** 
+ Helper routine to dump a byte array as dec values to Serial.
+*/
+void printDec(byte *buffer, byte bufferSize) {
+for (byte i = 0; i < bufferSize; i++) {
+ Serial.print(buffer[i] < 0x10 ? " 0" : " ");
+ Serial.print(buffer[i], DEC);
+ 
+}
+
+}
